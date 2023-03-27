7c6882c3d96034f87d814a05464439ea70b6dde7
SricharanSS <125631907+SricharanSS@users.noreply.github.com>
2023-03-27 13:47:26 +0530

    Remove Comments

--- js/index.js
+++ js/index.js
@@ -1,19 +1,16 @@
 const http = require("http");
 const fs = require("fs");
 
-// http.createServer(function(req,res,err) {
-    const colorCodes = JSON.parse(fs.readFileSync("assets/color_ palette.json","utf-8"));
-    const randomColorPalette = [];
-    for(let i=0; i<5; i++) {
-        let index = Math.floor(Math.random()*colorCodes.length);
-        randomColorPalette.push(colorCodes[index]);
-    }
+const colorCodes = JSON.parse(fs.readFileSync("assets/color_ palette.json","utf-8"));
+const randomColorPalette = [];
+for(let i=0; i<5; i++) {
+    let index = Math.floor(Math.random()*colorCodes.length);
+    randomColorPalette.push(colorCodes[index]);
+}
     
-    fs.writeFileSync("assets/randomized_color_palette.json", JSON.stringify(randomColorPalette));
+fs.writeFileSync("assets/randomized_color_palette.json", JSON.stringify(randomColorPalette));
 
-    const newColorCodes = JSON.parse(fs.readFileSync("assets/randomized_color_palette.json","utf-8"));
-    for(const element of newColorCodes) {
-        console.log(element.color);
-        // res.write("<h1 style='color : "+element.code.hex+";'> Color - "+element.color+"</h1>");
-    }
-// }).listen(4000);
+const newColorCodes = JSON.parse(fs.readFileSync("assets/randomized_color_palette.json","utf-8"));
+for(const element of newColorCodes) {
+    console.log(element.color);
+}
