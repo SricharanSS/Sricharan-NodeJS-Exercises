0157946d6b2445edc0989af778317bef13c073f9
SricharanSS <125631907+SricharanSS@users.noreply.github.com>
2023-03-27 12:56:06 +0530

    finish E1

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const fs = require("fs");
+
+http.createServer(function(req,res,err) {
+    const colorCodes = JSON.parse(fs.readFileSync("assets/color_ palette.json","utf-8"));
+    const randomColorPalette = [];
+    for(let i=0; i<5; i++) {
+        let index = Math.floor(Math.random()*colorCodes.length);
+        randomColorPalette.push(colorCodes[index]);
+    }
+    // console.log(randomColorPalette);
+    fs.writeFileSync("assets/randomized_color_palette.json", JSON.stringify(randomColorPalette));
+
+    const newColorCodes = JSON.parse(fs.readFileSync("assets/randomized_color_palette.json","utf-8"));
+    for(const element of newColorCodes) {
+        res.write("<h1 style='color : "+element.code.hex+";'> Color - "+element.color+"</h1>");
+    }
+}).listen(4000);
