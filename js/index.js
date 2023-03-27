c54dae146853f35cb2be46623384aea988aabf9d
SricharanSS <125631907+SricharanSS@users.noreply.github.com>
2023-03-27 14:06:44 +0530

    Add E2

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require("fs");
+
+http.createServer(function(req,res,err) {
+    const colorCodes = JSON.parse(fs.readFileSync("assets/color_ palette.json","utf-8", (err) => {
+        if(err) {
+            console.log("Cannot Read from File");
+        }
+    }));
+    const randomColorPalette = [];
+    let indexArray = [];
+    let count = 0;
+    while(count < 5) {
+        let index = Math.floor(Math.random()*colorCodes.length);
+        if(indexArray.indexOf(index) === -1) {
+            count++;
+            indexArray.push(index);
+            randomColorPalette.push(colorCodes[index]);
+        }
+    }
+    
+    fs.writeFileSync("assets/randomized_color_palette.json", JSON.stringify(randomColorPalette),(err)=> {
+        if(err) {
+            console.log("Cannot Write to File");
+        }
+    });
+
+    const newColorCodes = JSON.parse(fs.readFileSync("assets/randomized_color_palette.json","utf-8"));
+    for(const element of newColorCodes) {
+        res.write("<h1 style='text-align: center; color : "+element.code.hex+"; background-color: #000; border-radius: 5px;'> Color - "+element.color+"</h1>");
+    }
+}).listen(4000);
