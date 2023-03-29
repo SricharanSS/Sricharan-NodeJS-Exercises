d4a2737a1e18b946b744f2b88b1859593e365258
SricharanSS <125631907+SricharanSS@users.noreply.github.com>
2023-03-29 19:49:03 +0530

    Add comments

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 import { readFileSync, writeFileSync } from 'fs';
 
+// Read File 'color_palette.json' and store it a varibale
 const colorCodes = JSON.parse(readFileSync("assets/color_ palette.json","utf-8"));
+
+// Create an Array for storing Random Colors
 const randomColorPalette = [];
+
+// Create an array for getting unique random number
 let indexArray = [];
+
 for(let i=0; i<5; i++) {
     let index = Math.floor(Math.random()*colorCodes.length);
     if(indexArray.indexOf(index) === -1) {
@@ -10,10 +16,14 @@ for(let i=0; i<5; i++) {
         randomColorPalette.push(colorCodes[index]);
     }
 }
-    
+   
+// Write to 'randomized_color_palette.json'
 writeFileSync("assets/randomized_color_palette.json", JSON.stringify(randomColorPalette));
 
+// Read from 'randomized_color_palette.json' and store it in a variable
 const newColorCodes = JSON.parse(readFileSync("assets/randomized_color_palette.json","utf-8"));
+
+// Console Log the output
 for(const element of newColorCodes) {
     console.log(element.color);
 }
