2baa2458d24a49394c4ae95154d3f7dade8def23
SricharanSS <125631907+SricharanSS@users.noreply.github.com>
2023-04-07 10:09:51 +0530

    Make readFile synchoronous

--- js/index.js
+++ js/index.js
@@ -1,29 +1,51 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { readFile, readFileSync, writeFileSync } from 'fs';
 
 // Read File 'color_palette.json' and store it a varibale
-const colorCodes = JSON.parse(readFileSync("assets/color_ palette.json","utf-8"));
+function ReadFile() {
+    return new Promise( (resolve, reject) => {
+        readFile("assets/color_ palette.json","utf-8", (err, data) => {
+            if(err) {
+                reject("Can't Read File");
+            }
+            else {
+                resolve(data);
+            }
+        });
+    }).then(
+        function(data) {return data},
+        function(err) {return err}
+    );
+}
 
-// Create an Array for storing Random Colors
-const randomColorPalette = [];
+async function runFile() {
+    let data = await ReadFile();
+    let colorCodes = JSON.parse(data);
 
-// Create an array for getting unique random number
-let indexArray = [];
 
-for(let i=0; i<5; i++) {
-    let index = Math.floor(Math.random()*colorCodes.length);
-    if(indexArray.indexOf(index) === -1) {
-        indexArray.push(index);
-        randomColorPalette.push(colorCodes[index]);
+    // Create an Array for storing Random Colors
+    const randomColorPalette = [];
+
+    // Create an array for getting unique random number
+    let indexArray = [];
+
+    while(randomColorPalette.length != 5) {
+        let index = Math.floor(Math.random()*colorCodes.length);
+        if(indexArray.indexOf(index) === -1) {
+            indexArray.push(index);
+            randomColorPalette.push(colorCodes[index]);
+        }
+    }
+   
+    // Write to 'randomized_color_palette.json'
+    writeFileSync("assets/randomized_color_palette.json", JSON.stringify(randomColorPalette));
+
+    // Read from 'randomized_color_palette.json' and store it in a variable
+    const newColorCodes = JSON.parse(readFileSync("assets/randomized_color_palette.json","utf-8"));
+
+    // Console Log the output
+    for(const element of newColorCodes) {
+        console.log(element.color);
     }
 }
-   
-// Write to 'randomized_color_palette.json'
-writeFileSync("assets/randomized_color_palette.json", JSON.stringify(randomColorPalette));
 
-// Read from 'randomized_color_palette.json' and store it in a variable
-const newColorCodes = JSON.parse(readFileSync("assets/randomized_color_palette.json","utf-8"));
-
-// Console Log the output
-for(const element of newColorCodes) {
-    console.log(element.color);
-}
+runFile();
