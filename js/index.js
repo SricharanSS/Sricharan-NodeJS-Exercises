e02eb606ad0636db9a0f3a126b838b0d7507e20e
SricharanSS <125631907+SricharanSS@users.noreply.github.com>
2023-04-07 18:43:36 +0530

    Write as JSON

--- js/index.js
+++ js/index.js
@@ -5,31 +5,36 @@ http.createServer(function(req,res,err) {
     if(err) {
         console.log("Server Connection Problem : ",err);
     }
-    const colorCodes = JSON.parse(fs.readFileSync("assets/color_ palette.json","utf-8", (err) => {
-        if(err) {
-            console.log("Cannot Read from File");
+    else {
+        const colorCodes = JSON.parse(fs.readFileSync("assets/color_ palette.json","utf-8", (err) => {
+            if(err) {
+                console.log("Cannot Read from File");
+            }
+        }));
+        let randomColorPalette = [];
+        let indexArray = [];
+        let count = 0;
+        while(count < 5) {
+            let index = Math.floor(Math.random()*colorCodes.length);
+            if(indexArray.indexOf(index) === -1) {
+                count++;
+                indexArray.push(index);
+                randomColorPalette.push(colorCodes[index]);
+            }
         }
-    }));
-    const randomColorPalette = [];
-    let indexArray = [];
-    let count = 0;
-    while(count < 5) {
-        let index = Math.floor(Math.random()*colorCodes.length);
-        if(indexArray.indexOf(index) === -1) {
-            count++;
-            indexArray.push(index);
-            randomColorPalette.push(colorCodes[index]);
-        }
-    }
+        
+        fs.writeFileSync("assets/randomized_color_palette.json", JSON.stringify(randomColorPalette),(err)=> {
+            if(err) {
+                console.log("Cannot Write to File");
+            }
+        });
     
-    fs.writeFileSync("assets/randomized_color_palette.json", JSON.stringify(randomColorPalette),(err)=> {
-        if(err) {
-            console.log("Cannot Write to File");
-        }
-    });
-
-    const newColorCodes = JSON.parse(fs.readFileSync("assets/randomized_color_palette.json","utf-8"));
-    for(const element of newColorCodes) {
-        res.write("<h1 style='text-align: center; color : "+element.code.hex+"; background-color: #000; border-radius: 5px;'> Color - "+element.color+"</h1>");
+        const newColorCodes = JSON.parse(fs.readFileSync("assets/randomized_color_palette.json","utf-8"));
+       
+        res.write("<h1>New Color Codes</h1>");
+        res.write(JSON.stringify(newColorCodes));
+        // for(const element of newColorCodes) {
+        //     res.write("<h1 style='text-align: center; color : "+element.code.hex+"; background-color: #000; border-radius: 5px;'> Color - "+element.color+"</h1>");
+        // }
     }
-}).listen(4001);
+}).listen(4002);
