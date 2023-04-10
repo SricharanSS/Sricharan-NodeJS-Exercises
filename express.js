d90e5c5754d9de332dab590583c689b1ab38c8c9
SricharanSS <125631907+SricharanSS@users.noreply.github.com>
2023-04-10 17:34:09 +0530

    Add CORS

--- express.js
+++ express.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('./config/logger.config');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT;
+
+// CORS
+let corsOption = {origin : process.env.CORS_ORIGIN};
+app(cors(corsOption.origin));
 
 // To Get data from POST also
 app.use(express.urlencoded(
